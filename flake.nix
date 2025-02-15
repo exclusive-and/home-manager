efc94dca5e913640130e26d4872344fe85349198
exclusive-and <exclusiveandgate@gmail.com>
2025-02-15 14:12:04 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+    description = "Home Manager configuration of xand";
+
+    inputs = {
+        # Specify the source of Home Manager and Nixpkgs.
+        nixpkgs.url = "nixpkgs/nixos-24.11";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { nixpkgs, home-manager, ... }:
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            homeConfigurations."xand" = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+
+                # Specify your home configuration modules here, for example,
+                # the path to your home.nix.
+                modules = [ ./home.nix ];
+
+                # Optionally use extraSpecialArgs
+                # to pass through arguments to home.nix
+            };
+        };
+}
---
+++ home.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "xand";
+    home.homeDirectory = "/home/xand";
+
+    home.stateVersion = "24.11"; # Check the Home Manager release notes before changing.
+
+    home.packages = with pkgs; [
+        helvum
+        pavucontrol
+    ];
+
+    programs.home-manager.enable = true; # Let Home Manager install and manage itself.
+
+    programs.bash.enable = true;
+
+    programs.direnv = {
+        enable = true;
+        enableBashIntegration = true;
+        nix-direnv.enable = true;
+    };
+
+    programs.firefox = {
+        enable = true;
+        package = pkgs.firefox;
+    };
+
+    programs.fish.enable = true;
+
+    programs.git = {
+        enable = true;
+        ignores = [
+            ".direnv/"
+            "dist-newstyle/"
+            "obj_dir/"
+        ];
+        userEmail = "exclusiveandgate@gmail.com";
+        userName = "exclusive-and";
+    };
+
+    programs.vscode = {
+        enable = true;
+        enableExtensionUpdateCheck = true;
+        enableUpdateCheck = true;
+        package = pkgs.vscodium;
+        extensions = with pkgs.vscode-extensions; [
+            haskell.haskell
+            jnoortheen.nix-ide
+            llvm-vs-code-extensions.vscode-clangd
+            mkhl.direnv
+            mshr-h.veriloghdl
+        ];
+    };
+}
