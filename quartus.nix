f98ed0aad5582e44adee380093b8b84286798b7b
exclusive-and <exclusiveandgate@gmail.com>
2025-02-18 11:44:56 -0500

    Changed script to work properly

--- quartus.nix
+++ quartus.nix
@@ -1,19 +1,19 @@
 { config, pkgs, ... }:
 
 let
-    run-quartus = pkgs.writeShellScriptBin "quartus" ''
+    run-quartus = pkgs.writeShellScriptBin "run-quartus" ''
         if [ "$#" -ne 1 ]; then
-            >&2 echo "Usage: quartus <version>"
+            >&2 echo "Usage:    run-quartus <version>"
             >&2 echo ""
-            >&2 echo "  version: pro or lite"
+            >&2 echo "          version:    pro or lite"
         fi
 
         if [ "$1" = "pro" ]; then
-            ${pkgs.nix}/bin/nix run github:Mop-u/nix-quartus#quartus-prime-pro
+            ${pkgs.nix}/bin/nix shell github:Mop-u/nix-quartus#quartus-prime-pro --command "quartus"
         fi
 
         if [ "$1" = "lite" ]; then
-            ${pkgs.nix}/bin/nix run github:Mop-u/nix-quartus#quartus-prime-lite
+            ${pkgs.nix}/bin/nix shell github:Mop-u/nix-quartus#quartus-prime-lite --command "quartus"
         fi
     '';
 in
