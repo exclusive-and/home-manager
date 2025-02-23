8d3b047a5f0b70f6d3c00ccef98ff29ee75b5038
exclusive-and <exclusiveandgate@gmail.com>
2025-02-22 19:17:20 -0500

    Corrected username in flake

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
             system = "x86_64-linux";
             pkgs = import nixpkgs { inherit system; };
         in {
-            homeConfigurations."xand" = home-manager.lib.homeManagerConfiguration {
+            homeConfigurations."gaming" = home-manager.lib.homeManagerConfiguration {
                 inherit pkgs;
 
                 # Specify your home configuration modules here, for example,
