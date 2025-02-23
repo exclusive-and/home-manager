18681feb438cdbb5c1210f9e4840d7024b7e8210
exclusive-and <exclusiveandgate@gmail.com>
2025-02-22 19:06:29 -0500

    Updated formatting

--- programs/firefox/default.nix
+++ programs/firefox/default.nix
@@ -7,16 +7,16 @@ let
         "sidebar.verticalTabs" = true;
     };
 in
-    {
-        programs.firefox = {
-            enable = true;
-            package = pkgs.firefox;
-            profiles = {
-                default = {
-                    id = 0;
-                    path = "2d43dm8x.default";
-                    inherit settings;
-                };
+{
+    programs.firefox = {
+        enable = true;
+        package = pkgs.firefox;
+        profiles = {
+            default = {
+                id = 0;
+                path = "2d43dm8x.default";
+                inherit settings;
             };
         };
-    }
+    };
+}
