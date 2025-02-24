b6de912317f039be961a79bc4263e31e90a31a63
exclusive-and <exclusiveandgate@gmail.com>
2025-02-24 16:51:45 -0500

    Added overlays.nix

--- flake.nix
+++ flake.nix
@@ -2,23 +2,42 @@
     description = "Home Manager configuration of xand";
 
     inputs = {
-        # Specify the source of Home Manager and Nixpkgs.
         nixpkgs.url = "nixpkgs/nixos-24.11";
+
         home-manager = {
             url = "github:nix-community/home-manager/release-24.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
+        nurpkgs = {
+            url = "github:nix-community/NUR";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { nixpkgs, home-manager, ... }:
+    outputs = { nixpkgs, home-manager, ... } @ inputs:
         let
             system = "x86_64-linux";
+<<<<<<< HEAD
             pkgs = import nixpkgs {
                 inherit system;
                 config.allowUnfree = true;
             };
         in {
             homeConfigurations."gaming" = home-manager.lib.homeManagerConfiguration {
+=======
+
+            overlays = import ./overlays.nix {
+                inherit inputs system;
+            };
+
+            pkgs = import nixpkgs {
+                inherit overlays system;
+            };
+        in
+        {
+            homeConfigurations."xand" = home-manager.lib.homeManagerConfiguration {
+>>>>>>> 6757da7 (Added overlays.nix)
                 inherit pkgs;
 
                 # Specify your home configuration modules here, for example,
