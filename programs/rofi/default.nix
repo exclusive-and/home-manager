2c2587ec3ed119c3e98ec0866c6b073304dd32a1
exclusive-and <exclusiveandgate@gmail.com>
2025-02-25 22:16:54 -0500

    Fixed rofi transparency issues

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -1,11 +1,14 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+    inherit (config.home) homeDirectory;
+in
 {
     programs.rofi = {
         enable = true;
         plugins = with pkgs; [ rofi-emoji ];
         terminal = "${pkgs.alacritty}/bin/alacritty";
-        theme = ./theme.rafi;
+        theme = ./theme.rasi;
     };
 
     home.packages = [ pkgs.xdotool ];
